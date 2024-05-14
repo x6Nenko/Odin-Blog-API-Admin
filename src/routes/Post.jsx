cdf1b3bfdd5706999832d47bae4e997638de1722
Max I <clickkava@gmail.com>
2024-05-14 18:02:51 +0300

    Add ability to manage comments (edit, delete)

--- src/routes/Post.jsx
+++ src/routes/Post.jsx
@@ -6,8 +6,9 @@ import { useState } from "react";
 const Post = () => {
   const { postid } = useParams();
   const [refetchPost, setRefetchPost] = useState(false);
+  const [refetchComments, setRefetchComments] = useState(false);
   const postData = useData(`http://localhost:3000/posts/${postid}`, refetchPost);
-  const commentsData = useData(`http://localhost:3000/posts/${postid}/comments`);
+  const commentsData = useData(`http://localhost:3000/posts/${postid}/comments`, refetchComments);
   const commentsArray = commentsData && commentsData.posts;
 
   function convertTime(time) {
@@ -36,6 +37,74 @@ const Post = () => {
 
       if (response.ok) {
         setRefetchPost(!refetchPost);
+        console.log("ok");
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.error('Something went wrong:', error);
+      // Handle other error cases
+    }
+  }
+
+  // ---------------------------------------
+  // edit comments data
+  const [formData, setFormData] = useState({
+    text: '',
+  });
+
+  // if comment id is in the state then it means its in an edit process
+  const [isEdit, setIsEdit] = useState();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  function editCommentBtn(e, comment) {
+    setFormData({text: comment.text})
+    setIsEdit(comment._id);
+  }
+
+  function cancelEditBtn() {
+    setIsEdit(null);
+  }
+
+  async function deleteCommentBtn(e, comment) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${postid}/comments/${comment._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer " + localStorage.getItem("token")
+        },
+      });
+
+      if (response.ok) {
+        setRefetchComments(!refetchComments);
+        console.log("ok");
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.error('Something went wrong:', error);
+      // Handle other error cases
+    }
+  }
+
+  async function handleSubmitCommentEdit() {
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${postid}/comments/${isEdit}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer " + localStorage.getItem("token")
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
         console.log("ok");
       } else {
         console.log("not ok");
@@ -99,8 +168,23 @@ const Post = () => {
       <section className="comments-section">
         <h4>Comments</h4>
 
+        {isEdit && 
+          <form onSubmit={handleSubmitCommentEdit}>
+            <div className="input-holder">
+              <label htmlFor="text">Edit the comment:</label>
+              <textarea name="text" id="text" rows={3} value={formData.text} onChange={handleChange} required />
+            </div>
+
+            <button className="send-comment-btn" type="submit">Submit changes</button>
+            <button className="cancel-btn" onClick={cancelEditBtn}>Cancel</button>
+          </form>
+        }
+
         {commentsData && commentsArray.map(comment => ( 
-          <p className="comment" key={comment._id}>{comment.author.username}: {comment.text} <span>({convertTime(comment.createdAt)})</span></p>
+          <p className="comment" key={comment._id}>
+            {comment.author.username}: {comment.text} <span>({convertTime(comment.createdAt)})&nbsp;
+            <a href="#" onClick={(e) => editCommentBtn(e, comment)}>Edit</a> / <a href="#" onClick={(e) => deleteCommentBtn(e, comment)}>Delete</a></span>
+          </p>
         ))}
       </section>
     </main>
